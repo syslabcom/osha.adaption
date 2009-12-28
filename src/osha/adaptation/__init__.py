23ca5051185cb0e836fb2b190271bb65c67309d9
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-12-28 15:14:38 +0000

    Add the osha.adaptation source code. Taken and modified from osha.policy/adapter/*

---
+++ src/osha/adaptation/__init__.py
@@ -0,0 +1,4 @@
+import logging
+logger = logging.getLogger("osha.adaptation")
+
+import schemaextender
---
+++ src/osha/adaptation/subtyper.py
@@ -0,0 +1,16 @@
+import zope.component
+
+from p4a.subtyper import interfaces
+
+class IAnnotatedLinkList(zope.interface.Interface): 
+    """ Marker interface """
+
+class AnnotatedLinkListDescriptor(object):
+    zope.interface.implements(interfaces.IPortalTypedDescriptor)
+    title = u'Annotated Links'
+    description = \
+        u'Annotated links can be used by portlets to display additional ' \
+        u'information.'
+    type_interface = IAnnotatedLinkList
+    for_portal_type = 'Document'
+
