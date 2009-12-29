b67012bd88de658901d1defde7fabf54a1a93a76
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-12-29 14:50:21 +0000

    Add products required for testing

--- setup.py
+++ setup.py
@@ -3,44 +3,56 @@ import os
 
 version = '0.1'
 
-setup(name='osha.adaptation',
-      version=version,
-      description="OSHA specific adaptors, schemaextensions and subtypes",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "License :: OSI Approved :: European Union Public Licence 1.1 (EUPL 1.1)",
-        ],
-      keywords='plone osha',
-      author='Syslab.com GmbH',
-      author_email='info@syslab.com',
-      url='http://syslab.com',
-      license='GPL',
-      packages=['osha', 'osha/adaptation'],
-      package_dir = {'' : 'src'},
-      namespace_packages=['osha'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-            'setuptools',
-            'archetypes.schemaextender',
-            'Products.ATVocabularyManager',
-            'Products.DataGridField',
-            'Products.LinguaPlone',
-            'Products.OSHATranslations',
-            'Products.VocabularyPickerWidget',
-            'p4a.subtyper',
-            'slc.treecategories',
-      ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=["PasteScript"],
-      paster_plugins = ["ZopeSkel"],
-      )
+tests_require=[
+            'zope.testing',
+            'Products.TextIndexNG3',
+            'Products.CaseStudy',
+            'Products.RichDocument',
+            'Products.RALink',
+            'Products.PressRoom',
+            ]
+
+setup(
+    name='osha.adaptation',
+    version=version,
+    description="OSHA specific adaptors, schemaextensions and subtypes",
+    long_description=open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+    "Framework :: Plone",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "License :: OSI Approved :: European Union Public Licence 1.1 (EUPL 1.1)",
+    ],
+    keywords='plone osha',
+    author='Syslab.com GmbH',
+    author_email='info@syslab.com',
+    url='http://syslab.com',
+    license='GPL',
+    packages=['osha', 'osha/adaptation'],
+    package_dir = {'' : 'src'},
+    namespace_packages=['osha'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'archetypes.schemaextender',
+        'Products.ATVocabularyManager',
+        'Products.DataGridField',
+        'Products.LinguaPlone',
+        'Products.OSHATranslations',
+        'Products.VocabularyPickerWidget',
+        'p4a.subtyper',
+        'slc.treecategories',
+    ],
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    setup_requires=["PasteScript"],
+    paster_plugins = ["ZopeSkel"],
+    )
