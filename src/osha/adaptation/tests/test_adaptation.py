9936f5296069476d2f0491da26985c4230c0a406
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-12-29 16:37:05 +0000

    updated the schema fields ordering tests. Not working yet...

--- src/osha/adaptation/tests/test_adaptation.py
+++ src/osha/adaptation/tests/test_adaptation.py
@@ -1,8 +1,16 @@
 import unittest
 
+from zope.component import getAdapters
+
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
+
+from Products.Archetypes.utils import OrderedDict
+
 from base import OshaAdaptationTestCase
 
 from osha.adaptation.config import DEFAULT_FIELDS
+
 
 #    'Blob', no content type
 #    'RichDocument', ua
@@ -50,11 +58,36 @@ class TestSchemaExtender(OshaAdaptationTestCase):
         for type in EXTENDED_TYPES:
             obj = self.portal.get(type)
 
-            default_schema = obj.Schema().getSchemataFields("default")
+            schema = obj.Schema()
+            default_schema = schema.getSchemataFields("default")
             fields = [i.__name__ for i in default_schema]
+
+            sorted_returned_fields = fields
+            sorted_returned_fields.sort()
+
+            sorted_default_fields = DEFAULT_FIELDS[type]
+            sorted_default_fields.sort()
+
+            self.assertEquals(
+                sorted_default_fields,
+                sorted_returned_fields,
+                "The sorted fields of %s: %s do not match the sorted default "
+                "fields: %s" \
+                % (type, sorted_returned_fields, sorted_default_fields))
+        
+            original = OrderedDict()
+            for name in schema.getSchemataNames():
+                fields = schema.getSchemataFields(name)
+                original[name] = list(x.getName() for x in fields)
+
+            extenders = list(getAdapters((obj,), ISchemaExtender))
+            for name, extender in extenders:
+                if IOrderableSchemaExtender.providedBy(extender):
+                    ordered_returned_fields = extender.getOrder(original)
+
             self.assertEquals(
                 DEFAULT_FIELDS[type],
-                fields,
+                ordered_returned_fields,
                 "%s has the following Default fields: %s but should have %s"
                 %(type, fields, DEFAULT_FIELDS[type]))
 
