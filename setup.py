7c33978ca99e3c5d7ddc7e5ca01f7dd373fe1d63
jure <jure@97d050b9-b799-4550-a901-4f04598d4601>
2012-06-01 21:50:22 +0000

    Removed tests (moved to osha.policy)

--- setup.py
+++ setup.py
@@ -3,29 +3,15 @@ import os
 
 version = '0.1.15dev'
 
-tests_require=[
-            'zope.testing',
-            'Products.TextIndexNG3',
-            'Products.CaseStudy',
-            'Products.RichDocument',
-            'Products.RALink',
-            'Products.RemoteProvider',
-            'Products.PressRoom',
-            'Products.PloneHelpCenter',
-            'Products.OSHContentLink',
-            'osha.whoswho',
-            'plone.app.blob',
-            'slc.seminarportal',
-            ]
-
 setup(
     name='osha.adaptation',
     version=version,
     description="OSHA specific adaptors, schemaextensions and subtypes",
     long_description=open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read() +"\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read() + "\n" +
                     open(os.path.join("docs", "TODO.txt")).read(),
-    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
     "Framework :: Plone",
     "Programming Language :: Python",
@@ -39,7 +25,7 @@ setup(
     url='http://syslab.com',
     license='GPL',
     packages=['osha', 'osha/adaptation'],
-    package_dir = {'' : 'src'},
+    package_dir={'': 'src'},
     namespace_packages=['osha'],
     include_package_data=True,
     zip_safe=False,
@@ -53,12 +39,10 @@ setup(
         'Products.LinguaPlone',
         'p4a.subtyper',
     ],
-    tests_require=tests_require,
-    extras_require=dict(tests=tests_require),
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
     """,
     setup_requires=["PasteScript"],
-    paster_plugins = ["ZopeSkel"],
+    paster_plugins=["ZopeSkel"],
     )
